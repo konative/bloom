c6f077651a0eae034237a782c30c4c833e86b344
tariqqureshey <tariqqureshey@gmail.com>
2021-01-24 04:40:41 -0500

    Added Container to Test
    Testing Container

--- client/src/components/form/form.js
+++ client/src/components/form/form.js
@@ -24,18 +24,20 @@ function Form() {
     }
 
     return (
-        <form onSubmit={handleSubmit}>
-            <input placeholder='Please enter your business name' value={busName} onChange={e => setBusName(e.target.value)} required />
-            <br />
-            <input placeholder='Please enter your phone number' value={phoneNum} onChange={e => setPhoneNum(e.target.value)} required />
-            <input placeholder='Please enter your address' value={address} onChange={e => setAddress(e.target.value)} required />
-            <br />
-            <input placeholder='Please enter a short description about your business' value={desc} onChange={e => setDesc(e.target.value)} required />
-            <br />
-            <input type="checkbox" onChange={e => setTermsAndCon(e.target.value)} required />
-            <br />
-            <button>Submit</button>
-        </form>
+        <container>
+            <form onSubmit={handleSubmit}>
+                <input placeholder='Please enter your business name' value={busName} onChange={e => setBusName(e.target.value)} required />
+                <br />
+                <input placeholder='Please enter your phone number' value={phoneNum} onChange={e => setPhoneNum(e.target.value)} required />
+                <input placeholder='Please enter your address' value={address} onChange={e => setAddress(e.target.value)} required />
+                <br />
+                <input placeholder='Please enter a short description about your business' value={desc} onChange={e => setDesc(e.target.value)} required />
+                <br />
+                <input type="checkbox" onChange={e => setTermsAndCon(e.target.value)} required />
+                <br />
+                <button>Submit</button>
+            </form>
+        </container>
     );
 }
 
