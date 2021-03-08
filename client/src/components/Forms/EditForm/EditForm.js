a5c44acaa4f5bf3e89191df1e12d8e158cd2c4d5
tariqqureshey <tariqqureshey@gmail.com>
2021-03-07 23:32:13 -0500

    Values Now Preload on to Correct Input
    Value of Owner's business name, phone, etc. load in to the inputs, so it is easier for user to reference what to edit.

--- client/src/components/Forms/EditForm/EditForm.js
+++ client/src/components/Forms/EditForm/EditForm.js
@@ -1,8 +1,26 @@
 import {React, useState, useEffect, useForm} from "react";
-import { Redirect, Link, useHistory } from "react-router-dom";
+import { Redirect, Link, useHistory, useLocation } from "react-router-dom";
 import "./EditForm.css";
+import ListingCard from "../../ListingCard/ListingCard.js";
 
 function EditForm(props){
+
+  const location = useLocation();
+  console.log(location);
+  let id = location.pathname.replace("/edit/", "");
+  useEffect(async () => {
+    await fetch(`http://localhost:5000/listings/${id}`)
+      .then((response) => response.json())
+      .then((data) => {
+        if (!data.notfound) {
+          //When data found
+          setBusName(data.name);
+          setPhoneNum(data.phoneNum);
+          setAddress(data.address);
+          setDesc(data.description);
+        }
+      });
+  }, []);
 
   const [busName, setBusName] = useState("");
   const [phoneNum, setPhoneNum] = useState("");
@@ -15,7 +33,6 @@ function EditForm(props){
         <div>
         <form onSubmit={handleSubmit} className="inputs">
         <input
-
           value={busName}
           onChange={(e) => setBusName(e.target.value)}
           required
