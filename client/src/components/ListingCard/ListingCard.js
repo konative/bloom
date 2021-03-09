5f9a2398dcf812c82aa5b388681867da3bb97a18
tariqqureshey <tariqqureshey@gmail.com>
2021-03-08 22:10:37 -0500

    Fixed Listing Page
    Listing shows again when clicking on any listing

--- client/src/components/ListingCard/ListingCard.js
+++ client/src/components/ListingCard/ListingCard.js
@@ -3,34 +3,33 @@ import { Redirect, Link } from "react-router-dom";
 import "./ListingCard.css";
 
 function ListingCard(props) {
-
   const [redirect, setRedirect] = useState(false);
   const redirectHandler = () => {
     setRedirect(true);
-  }
+  };
   const renderRedirect = () => {
     if (redirect) {
-      return <Redirect push to = {`/listing/${props.id}`} />
+      return <Redirect push to={`/listing/${props.id}`} />;
     }
-  }
+  };
 
-  const edit=props.edit
+  const edit = props.edit;
 
   if (edit) {
     return (
-    <div className="card" onClick={redirectHandler} >
-      {renderRedirect()}
-      <h1>{props.name}</h1>
-      <h2>Description: {props.description}</h2>
-      <h3>Contact: {props.phoneNum}</h3>
-      <h3>Address: {props.address}</h3>
-      <Link to ={`/edit/${props.id}`}>Edit</Link>
-    </div>
-    )
+      <div className="card" onClick={redirectHandler}>
+        {renderRedirect()}
+        <h1>{props.name}</h1>
+        <h2>Description: {props.description}</h2>
+        <h3>Contact: {props.phoneNum}</h3>
+        <h3>Address: {props.address}</h3>
+        <Link to={`/edit/${props.id}`}>Edit</Link>
+      </div>
+    );
   }
 
   return (
-    <div className="card" onClick={redirectHandler} >
+    <div className="card" onClick={redirectHandler}>
       {renderRedirect()}
       <h1>{props.name}</h1>
       <h2>Description: {props.description}</h2>
